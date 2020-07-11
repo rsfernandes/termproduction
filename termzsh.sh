9fb9c86579df7e6d7e1479a354d557bb757a0986
rsfernandes <64504126+rsfernandes@users.noreply.github.com>
2020-07-11 15:55:53 -0300

    Create termzsh.sh

---
+++ termzsh.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+distri=`cat /etc/*release|egrep -i "ubuntu|debian|centos|red|fedora"|head -1|cut -d= -f2|tr A-Z  a-z`
+if [ $distri == "ubuntu" || $distri == "debian" ]
+  then
+
+ if [ $USER != "root" ]
+   then
+
+     sudo apt install zsh -y
+     sudo apt install git -y
+     sudo apt install wget -y
+     cd ~
+     curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh; zsh
+     sudo usermod --shell $(which zsh) $USER
+     git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+     git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
+     wget --no-check-certificate https://github.com/rsfernandes/termproduction/blob/master/.zshrc -O ~/.zshrc
+     wget --no-check-certificate https://github.com/rsfernandes/termproduction/blob/master/.vimrc -O ~/.vimrc
+  else
+     apt install zsh -y
+     apt install git -y
+     apt install wget -y
+     cd ~
+     curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh; zsh
+     sudo usermod --shell $(which zsh) $USER
+     git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+     git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
+     wget --no-check-certificate https://github.com/rsfernandes/termproduction/blob/master/.zshrc -O ~/.zshrc
+     wget --no-check-certificate https://github.com/rsfernandes/termproduction/blob/master/.vimrc -O ~/.vimrc
+  fi
+elif [ $distri == "fedora" || $distri == "centos" ]
+  then
+   if [ $USER != "root" ]
+   then
+
+     sudo yum install git -y
+     sudo yum install zsh -y
+     sudo yum install wget -y
+     cd ~
+     curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh; zsh
+     sudo usermod --shell $(which zsh) $USER
+     git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+     git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
+     wget --no-check-certificate https://github.com/rsfernandes/termproduction/blob/master/.zshrc -O ~/.zshrc
+     wget --no-check-certificate https://github.com/rsfernandes/termproduction/blob/master/.vimrc -O ~/.vimrc
+   else
+     yum zsh -y
+     yum install git -y
+     yum install wget -y
+     cd ~
+     curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh; zsh
+     sudo usermod --shell $(which zsh) $USER
+     git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+     git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
+     wget --no-check-certificate https://github.com/rsfernandes/termproduction/blob/master/.zshrc -O ~/.zshrc
+     wget --no-check-certificate https://github.com/rsfernandes/termproduction/blob/master/.vimrc -O ~/.vimrc
+   fi  
+fi
