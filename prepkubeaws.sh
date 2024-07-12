9c5f97426bedc617c3b9ec478a6f59e9ba61901c
rsfernandes <64504126+rsfernandes@users.noreply.github.com>
2024-07-12 14:07:17 -0300

    Update prepkubeaws.sh

--- prepkubeaws.sh
+++ prepkubeaws.sh
@@ -12,5 +12,4 @@ sudo mv /tmp/eksctl /usr/local/bin
 printf "\n Instala kubetcl \n"
 curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.30.0/2024-05-12/bin/linux/amd64/kubectl
 chmod +x ./kubectl
-mv ./kubectl /usr/sbin/
 sudo mv ./kubectl /usr/sbin/
