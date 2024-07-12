124d6481f8bb289cb3666f69573f184c2e6ba746
rsfernandes <64504126+rsfernandes@users.noreply.github.com>
2024-07-12 14:04:25 -0300

    Create prepkubeaws.sh

---
+++ prepkubeaws.sh
@@ -0,0 +1,16 @@
+printf "Instala awscli \n"
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+aws --version
+printf "\n Instala eksctl \n"
+ARCH=amd64
+PLATFORM=$(uname -s)_$ARCH
+curl -sLO "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_$PLATFORM.tar.gz"
+tar -xzf eksctl_$PLATFORM.tar.gz -C /tmp && rm eksctl_$PLATFORM.tar.gz
+sudo mv /tmp/eksctl /usr/local/bin
+printf "\n Instala kubetcl \n"
+curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.30.0/2024-05-12/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+mv ./kubectl /usr/sbin/
+sudo mv ./kubectl /usr/sbin/
